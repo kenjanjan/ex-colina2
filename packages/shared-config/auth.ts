6a0e832a4a90e68648ad74d492a9e89dfb91bf98
kenjanjan <kjliloc@hcdc.edu.ph>
2025-01-15 14:27:28 +0800

    sso working

--- packages/shared-config/auth.ts
+++ packages/shared-config/auth.ts
@@ -3,4 +3,15 @@ import Keycloak from "next-auth/providers/keycloak";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Keycloak],
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async session({ session, user }) {
+      if (user) {
+        session.user = user;
+      }
+      return session;
+    },
+  },
 });
